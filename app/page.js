82d3dff1a44b5f26f80217b6c12f0693bebd40b4
Duong Xuan Anh <977-duongx@users.noreply.localhost>
2024-08-02 10:22:47 +0200

    App layout update

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <div>
-      Home page
+
+    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+      <div>Search</div>
+      <div className="col-span-2">Google Map</div>
     </div>
+
   );
 }
